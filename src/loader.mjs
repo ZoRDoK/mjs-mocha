aeea6b5262407392c009e3d5394e6c315e4787df
slava <zordok2@gmail.com>
2020-09-02 18:46:40 +0300

    fix errors

--- src/loader.mjs
+++ src/loader.mjs
@@ -5,16 +5,22 @@ const { URL } = url;
 
 // eslint-disable-next-line import/prefer-default-export
 export function resolve(specifier, context, defaultResolve) {
+  if (!context) {
+    return defaultResolve(specifier, context, defaultResolve);
+  }
+
   // it was changed in 12 version
   let { parentURL: base = null } = context;
 
-  if (base) {
-    // Convert parentModuleURL to system separator, remove leading backslash
-    const normalizedBase = path.join(new URL(base).pathname).replace(/^\\/, '');
-    if (normalizedBase === process.env.MOCHA_COPY_FILE) {
-      base = `file://${process.env.MOCHA_BASE_FILE}`;
-    }
+  if (!base) {
+    return defaultResolve(specifier, context, defaultResolve);
   }
 
-  return defaultResolve(specifier, base);
+  // Convert parentModuleURL to system separator, remove leading backslash
+  const normalizedBase = path.join(new URL(base).pathname).replace(/^\\/, '');
+  if (normalizedBase === process.env.MOCHA_COPY_FILE) {
+    context.parentURL = `file://${process.env.MOCHA_BASE_FILE}`;
+  }
+
+  return defaultResolve(specifier, context, defaultResolve);
 }
